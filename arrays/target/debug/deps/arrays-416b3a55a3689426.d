9082b081b227818b6bfdff570bfab25aae7dbd9c
Ukeme Edet <ukemeedet2207@gmail.com>
2023-04-21 17:59:19 +0100

    Learning Rust

---
+++ arrays/target/debug/deps/arrays-416b3a55a3689426.d
@@ -0,0 +1,5 @@
+/home/ukeme/Desktop/rust/arrays/target/debug/deps/arrays-416b3a55a3689426: src/main.rs
+
+/home/ukeme/Desktop/rust/arrays/target/debug/deps/arrays-416b3a55a3689426.d: src/main.rs
+
+src/main.rs:
