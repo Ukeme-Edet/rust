c7cce0c9239897835b58b4a073b7b1ae7c64f34e
Ukeme Edet <ukemeedet2207@gmail.com>
2023-04-21 18:01:39 +0100

    Learning Rust

---
+++ functions/target/debug/deps/functions-2d049428bafbea1b.d
@@ -0,0 +1,5 @@
+/home/ukeme/Desktop/rust/functions/target/debug/deps/functions-2d049428bafbea1b: src/main.rs
+
+/home/ukeme/Desktop/rust/functions/target/debug/deps/functions-2d049428bafbea1b.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/ukeme/Desktop/rust/functions/target/debug/functions: /home/ukeme/Desktop/rust/functions/src/main.rs
