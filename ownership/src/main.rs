b4f2de5b1f41a375ecadd6c9697d736453f85b85
Ukeme Edet <ukemeedet2207@gmail.com>
2023-05-07 19:32:49 +0100

    Ownership and Functions

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,37 @@
+// fn main() {
+// S is not valid here
+// let _s: String = String::from("hello"); // s is valid from this point foward
+
+// do stuff with s
+
+// let s1: String = String::from("hello");
+// let s2: String = s1.clone();
+// println!("{}, world!", s1);
+// println!("s1 = {}, s2 = {}", s1, s2);
+
+// } // this scope is now over, and s is no longer valid
+
 fn main() {
-    // S is not valid here
-    let _s: String = String::from("hello"); // s is valid from this point foward
+    let s: String = String::from("hello"); // s comes into scope
 
-    // do stuff with s
+    takes_ownership(s); // s's value moves into the function...
+                        // ... and is no longer valid here
 
-    // let s1: String = String::from("hello");
-    // let _s2: String = s1;
-    // println!("{}", s1);
-} // this scope is now over, and s is no longer valid
+    let x: i32 = 5; // x comes into scope
+
+    makes_copy(x); // x would move into the function,
+                   // but i32 is Copy, so it's okay to
+                   //still use x afterward
+} // Here, x goes out of scope, then s. Bit because s's value was moved,
+  // nothing special happens
+
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memeory is freed.
+
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope, Nothing special happens
